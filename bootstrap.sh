7a4e2791cf6799fa0618a2cdd630a2833c19511b
Billy Bromell <bbromell@amazon.co.uk>
2025-07-22 18:25:41 +0100

    feat: add zsh + tmux setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# zsh setup
 sudo chsh -s $(which zsh) $USER
+echo "ZDOTDIR=\$HOME/.config/zsh" >> ~/.zshenv
+git clone https://github.com/b177y/zsh_config ~/.config/zsh
 
+# neovim setup
 git clone https://github.com/b177y/astronvim_config ~/.config/nvim
 nvim --headless +q
+
+# tmux setup
+git clone https://github.com/b177y/tmux_config ~/.config/tmux
