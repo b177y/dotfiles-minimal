421e97e6a7e6f6c2143399064009c5f3c6d870ad
Billy Bromell <bbromell@amazon.co.uk>
2025-08-04 08:36:16 +0100

    feat: make scripts more idempotent

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+set -e
+
 # zsh setup
 sudo chsh -s $(which zsh) $USER
-echo "ZDOTDIR=\$HOME/.config/zsh" >> ~/.zshenv
-git clone https://github.com/b177y/zsh_config ~/.config/zsh
+
+if ! test -d ~/.config/zsh; then
+  echo "export ZDOTDIR=\$HOME/.config/zsh" >> ~/.zshenv
+  git clone https://github.com/b177y/zsh_config ~/.config/zsh
+  echo "export PATH=\"\$PATH:/opt/nvim-linux-x86_64/bin\"" >> ~/.config/zsh/.zshrc
+fi
 
 # neovim setup
-git clone https://github.com/b177y/astronvim_config ~/.config/nvim
-nvim --headless +q
+if ! test -d ~/.config/nvim; then
+  git clone https://github.com/b177y/astronvim_config ~/.config/nvim
+  source ~/.config/zsh/.zshrc
+  nvim --headless +q
+fi
 
 # tmux setup
-git clone https://github.com/b177y/tmux_config ~/.config/tmux
+if ! test -d ~/.config/tmux; then
+  git clone https://github.com/b177y/tmux_config ~/.config/tmux
+fi
--- install/deb.sh
+++ install/deb.sh
@@ -4,5 +4,6 @@ sudo apt install -y zsh
 sudo apt install -y git
 sudo apt install -y tmux
 
+rm -f /tmp/nvim.tar.gz
 curl -fLo /tmp/nvim.tar.gz https://github.com/neovim/neovim/releases/download/v0.11.3/nvim-linux-x86_64.tar.gz
 sudo tar -C /opt -xzf /tmp/nvim.tar.gz
